5f3f5e6785c0ca0de65f5d024e4f49b77f5753a5
unknown <jamesjhn19@gmail.com>
2024-02-14 10:30:24 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './componants/Navbar'
+import './index.css'
+
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
